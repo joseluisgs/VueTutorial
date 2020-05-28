f9abaea0b42ef4c4b7dc195caf7e32a27650b6c7
joseluisgs <joseluisgonsan@gmail.com>
2020-05-28 12:56:34 +0200

    06-Instancias

---
+++ 01-Esencial/06-Instancias/js/main.js
@@ -0,0 +1,32 @@
+// Estado de una instancia y comunicación entre varias
+const vm1 = new Vue({
+    el: 'main',
+    data: {
+        mensaje: 'Instancia Vue 1',
+    },
+    // antes de actualizarme
+    beforeUpdate() {
+        console.log('BeforeUpdate: ', this.mensaje);
+    },
+    // Al actualizarme
+    updated() {
+        console.log('Update: ', this.mensaje);
+    },
+    methods: {
+        alReves() {
+            this.mensaje = this.mensaje.split('').reverse().join('');
+        }
+    },
+    computed: {
+        mensajeMayusculas() {
+            return this.mensaje.toUpperCase();
+        }
+    }
+});
+
+const vm2 = new Vue({
+    el: '#app',
+    data: {
+        mensaje: 'Instancia Vue 2',
+    }
+});
