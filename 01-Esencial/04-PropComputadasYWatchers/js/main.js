9bd1a437e7076808a87bc4486687daf2fd6cb7e9
joseluisgs <joseluisgonsan@gmail.com>
2020-05-28 10:59:02 +0200

    04-PropComputadasYWatchers

---
+++ 01-Esencial/04-PropComputadasYWatchers/js/main.js
@@ -0,0 +1,67 @@
+// Creamos la instancia de Vue.
+// Le decimos que su campo de acción es todo lo que este entre las eqtiteas main, lo de fuera no lo entiende
+// Su modelo de dato es data
+// Vue.js
+new Vue({
+    // Elemento donde nos inyectamos
+    el: 'main',
+    // Modelo de datos
+    data: {
+        mensaje: 'Hola Mundo :) !',
+        nuevaTarea: '',
+        tareas: [{
+            titulo: 'Aprender Vue.js',
+            prioridad: true,
+            antiguedad: 23
+        },
+        {
+            titulo: 'Aprender ES6',
+            prioridad: false,
+            antiguedad: 135
+        },
+        {
+            titulo: 'Publicar algo todos los días',
+            prioridad: true,
+            antiguedad: 378
+        },
+        ],
+        error: ''
+    },
+    // Métodos
+    methods: {
+        agregarTarea() {
+            // this, hace referencia a la instancia Vue
+            this.tareas.unshift({
+                titulo: this.nuevaTarea,
+                prioridad: false,
+                antiguedad: 0,
+            });
+            this.nuevaTarea = '';
+        },
+    },
+    // Prpiedades computadas, es dcir, realiza operaciones de con elementos de nuestro modelo automáticamente antes de renderizarlos
+    computed: {
+        tareasConPrioridad() {
+            return this.tareas.filter((tarea) => tarea.prioridad);
+        },
+        mensajeAlReves() {
+            if (this.nuevaTarea.length != 0)
+                return this.nuevaTarea.split('').reverse().join('');
+        },
+        tareasPorAntiguedad() {
+            return this.tareas.sort((a, b) => b.antiguedad - a.antiguedad);
+        }
+    },
+    // Cuando tiene algunos datos que necesitan cambiarse en función de otros datos, es tentador utilizar watch en exceso, 
+    // especialmente si proviene de tener experiencia en AngularJS. 
+    // Sin embargo, a menudo es una mejor idea usar una propiedad computada en lugar de una imperativa llamada a watch
+     watch: {
+        nuevaTarea() {
+             if (this.nuevaTarea.length < 5) {
+                this.error = 'Nombre de tarea demasiado corto';
+            } else {
+                this.error = '';
+            }
+        }
+    }
+});
