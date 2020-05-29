a371f9c920b75cbbc6650958c3b9752840badb04
joseluisgs <joseluisgonsan@gmail.com>
2020-05-29 10:45:26 +0200

    04-Eventos

--- 02-Componentes/04-Eventos/js/main.js
+++ 02-Componentes/04-Eventos/js/main.js
@@ -1,11 +1,9 @@
 Vue.component('alerta', {
-    // Aquí con propiedades 
     props: ['tipo', 'posicion'],
-    // Con el slot podemos configurar que el template sea dinámico 
-    // Slots anónimo solo puede haber 1, los demás deben estar identificados, tener un name
     template: `
             <section class="alerta" :class="[tipo, posicion]">
                 <header class="alerta__header">
+                        <a href="#" @click="ocultarWidget">Cerrar</a>
                         <slot name="header">Hola</slot>
                 </header>
                 <div class="alerta__contenido">
@@ -14,30 +12,22 @@ Vue.component('alerta', {
                             vitae.
                         </slot>
                 </div>
-                <!-- El texto me llega en slot en la variable footer -->
                 <footer class="alerta__footer">
                         <slot name="footer">Este es el texto del footer</slot>
                 </footer>
             </section>`,
+    methods: {
+        ocultarWidget() {
+            this.$emit('ocultar');
+        }
+    }
 });
-
-// Otro componente. Los Scopd Slots nos permiten psar variables del modelo para personalizar los templates 
-// Es decir, incluir referencias al modelo o a la fuente de datos para un mayor nivel de abstracción
-Vue.component('mis-tareas', {
-    props: ['listado'],
-    template: '#mis-tareas-template' // Mi template
-});
-
 
 new Vue({
     el: 'main',
     data: {
-        tareas: [
-            { titulo: 'Salir a correr' },
-            { titulo: 'Recoger la casa' },
-            { titulo: 'Aprender Vue.js' },
-            { titulo: 'Ir al gimnasio' },
-            { titulo: 'Leer cada día' },
-        ]
+        mostrarExito: false,
+        mostrarError: false,
+        mostrarAdvertencia: false,
     }
 });
