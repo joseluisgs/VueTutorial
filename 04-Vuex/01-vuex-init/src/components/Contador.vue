af949b5963e8569abcc72091ab3a4c5d92165e7c
joseluisgs <joseluisgonsan@gmail.com>
2020-06-01 14:32:41 +0200

    05-Acciones

--- 04-Vuex/01-vuex-init/src/components/Contador.vue
+++ 04-Vuex/01-vuex-init/src/components/Contador.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
       <h1>Contador: {{ cantidad }}</h1>
-      <button @click="aumentar">Aumentar</button>
-      <button @click="reducir">Reducir</button>
+      <button @click="aumentar(1)">Aumentar</button>
+      <button @click="reducir(1)">Reducir</button>
+      <button @click="aumentarAsync(12)">Aumentar Async</button>
+      <button @click="reducirAsync({cantidad: 15})">Reducir Async</button>
     </div>
 </template>
 <script>
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
   name: 'Contador',
   // props: ['cantidad'], Ya no la necesitamos
-  methods: mapMutations(['aumentar', 'reducir']),
+  methods: { ...mapMutations(['aumentar', 'reducir']), ...mapActions(['aumentarAsync', 'reducirAsync']) },
   // methods: {
   //   aumentar() {
   //     // this.$emit('aumentar');
--- 04-Vuex/01-vuex-init/src/store/index.js
+++ 04-Vuex/01-vuex-init/src/store/index.js
@@ -21,23 +21,35 @@ export default new Vuex.Store({
       { nombre: 'Diseñar acciones de marketing', completado: true },
     ],
   },
-  // Getters, podemos obtener elemento computados del estado. Se realiza en único lugar:
+  // Getters, obtenemos elementos del estado. Podemos obtener elemento computados del estado.
+  // Se realiza en único lugar:
   // https://vuex.vuejs.org/guide/getters.html
   getters: {
     tareasCompletadas: (state) => state.tareas.filter((tarea) => tarea.completado).length,
   },
   // Funciones que cambian el estado, encapsulan como acceder al estado y de qué manera
   // https://vuex.vuejs.org/guide/mutations.html
+  // Pueden ser los setters de nuestro modelo :)
   mutations: {
-    aumentar(state) {
-      state.cantidad += 1;
+    aumentar: (state, cantidad) => {
+      state.cantidad += cantidad;
     },
-    reducir(state) {
-      state.cantidad -= 1;
+    reducir: (state, cantidad) => {
+      state.cantidad -= cantidad;
     },
   },
-  // funciones que confirman los cambios del estado
+  // funciones que confirman los cambios del estado deben ser asíncronos
+  // Para las acciones asíncronas debemos usar actions. Le hacen un commit a las mutaciones
+  // no se comunican directamente con el estado, sino que conectan con las mutations
+  // https://vuex.vuejs.org/guide/actions.html
+  // Pongo dos sintaxis diferentes, pero lo importante es el commit
   actions: {
+    aumentarAsync: (context, cantidad) => {
+      setTimeout(() => context.commit('aumentar', cantidad), 2000);
+    },
+    reducirAsync: ({ commit }, { cantidad }) => {
+      setTimeout(() => commit('reducir', cantidad), 2000);
+    },
   },
   // Agrupar e estado en cosas
   modules: {
