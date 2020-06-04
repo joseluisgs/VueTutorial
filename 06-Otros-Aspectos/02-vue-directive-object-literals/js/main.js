c16dc9e163b4d522b36d9887e130a0151afed04c
joseluisgs <joseluisgonsan@gmail.com>
2020-06-04 10:22:42 +0200

    02-Vue Directives Object Literals

--- 06-Otros-Aspectos/02-vue-directive-object-literals/js/main.js
+++ 06-Otros-Aspectos/02-vue-directive-object-literals/js/main.js
@@ -1,19 +1,14 @@
-// https://vuejs.org/v2/guide/custom-directive.html
-// Son directivas personalizadas creadas para compartir y personalizar y reutilizarlas
+// https://vuejs.org/v2/guide/custom-directive.html#Object-Literals
+
 Vue.directive('fijar', {
-    // Cuando exista el bind, puesto
-    // el es elemento donde lo vamos a apliacra
-    // binding es la directivadir
     bind(el, binding) {
-        console.log(el,binding);
-        // Lo dejamos fijado...
-        el.style.position = 'fixed';
+        console.log(binding.value);
+/*        el.style.position = 'fixed';
 
         if (binding.expression) {
             el.style.top = binding.expression + 'px';
         }
 
-        // si tenemos distintos argumentos 
         if (binding.arg) {
             let color = 'goldenrod';
             let modificadores = Object.keys(binding.modifiers);
@@ -21,7 +16,7 @@ Vue.directive('fijar', {
                 color = modificadores[0];
             }
             el.style.backgroundColor = color;
-        }
+        }*/
     }
 });
 
